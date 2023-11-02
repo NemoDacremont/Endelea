349de6e6302ea1f8bbade2de8207131f64ec752e
Nemo D'acremont <nemo.d'acremont@enseirb-matmecat.fr>
2023-11-02 11:11:51 +0100

    Added prejump and super actions handlers

--- player/player.gd
+++ player/player.gd
@@ -14,8 +14,6 @@ const COYOTE_JUMP_DURATION: float = 0.15
 var is_coyote_jump_available: bool = false
 var has_just_left_floor: bool = false
 
-enum actions {}
-
 const HEIGHT = 48
 const WIDTH = 48
 
@@ -23,14 +21,36 @@ const WIDTH = 48
 var is_moving_right: bool = false
 var is_moving_left: bool = false
 
-## Super Actions
-enum Super_Actions {JUMP_SUPER_ACTION}
-const super_action_names = ["jump"]
+#
+# Super Actions
+#
+enum Super_Actions {JUMP_SUPER_ACTION, PRE_JUMP, IS_JUMPING, NO_SUPER_ACTION}
+const super_action_names: Array[String] = ["Jump", "Pre jump", "Is jumping", "No supper action"]
+const SUPER_ACTION_BUFFER_TIMER_DURATION: float = 0.2
+
+const PRE_JUMP_DURATION: float = 0.025  # in sec
+const PRE_JUMP_VELOCITY_MULTIPLIER: float = 0.5
+const PRE_JUMP_PART1_DURATION: float = PRE_JUMP_DURATION / 2
+const PRE_JUMP_PART2_DURATION: float = PRE_JUMP_DURATION / 2
+
+var pre_jump_part1_time: float = 0
+var pre_jump_part2_time: float = 0
+
+var pre_jump_part: int = 0  # 0 indexed
+var pre_jump_part_count: int = 2
+
+var is_jump_available: bool = true
+
+var next_action: Super_Actions = Super_Actions.NO_SUPER_ACTION
+var super_action_buffer: Super_Actions = Super_Actions.NO_SUPER_ACTION
+
+@onready var super_action_buffer_timer: Timer = $SuperActionBufferTimer
 
 
 #
 # Physics
 #
+var tmp_velocity: Vector2 = Vector2.ZERO
 var acceleration: Vector2 = Vector2.ZERO
 
 ## direction = 1 if moving right, -1 if moving left
@@ -39,7 +59,7 @@ var direction: int = 1
 
 ## Axe x
 @export var MAX_SPEED_X: float = 3 * WIDTH  # 4 * WIDTH per second
-@export var time_full_speed = 0.1
+@export var time_full_speed: float = 0.1
 var AIR_FRICTION_X: float = 3 / time_full_speed
 
 var ACCEL_X: float = MAX_SPEED_X * AIR_FRICTION_X
@@ -58,6 +78,7 @@ const JUMP_VELOCITY: float = - 4 * MAX_HEIGHT_JUMP / JUMP_DURATION  # calculus s
 const IDLE_ANIMATION_NAME: String = "Idle"
 const WALK_ANIMATION_NAME: String = "Walk"
 const JUMP_ANIMATION_NAME: String = "Jump"
+const LAND_ANIMATION_NAME: String = "Land"
 
 const JUMP_ANIMATION_EPSILON: float = abs(JUMP_VELOCITY / 3)
 
@@ -89,12 +110,14 @@ func capture_inputs():
 	if Input.is_action_pressed(MOVE_LEFT_ACTION_NAME):
 		is_moving_left = true
 
+	super_action_capture_inputs()
+
 
 
 #
 # Procédures Animations
 #
-func animation_process():
+func animation_process(delta):
 
 	animation = IDLE_ANIMATION_NAME
 
@@ -118,22 +141,84 @@ func animation_process():
 
 		animation_node.frame = animationFrame
 
-
-
 	animation_node.play(animation)
+
+	super_action_animations_process(delta)
 
 
 #
 # Super Actions Handling
 #
-func super_action_physics_process(action: Super_Actions, _delta: float):
 
-	match (action):
+func get_next_action():
+	next_action = super_action_buffer
+	super_action_buffer = Super_Actions.NO_SUPER_ACTION
+
+
+func super_action_capture_inputs():
+	if (Input.is_action_just_pressed(JUMP_ACTION_NAME)):
+		if (is_on_floor() or is_coyote_jump_available):
+			print("Pre jump input")
+			if (next_action == Super_Actions.NO_SUPER_ACTION):
+				print("Pre jump next action")
+				next_action = Super_Actions.PRE_JUMP
+			else:
+				print("Pre jump buffered")
+				super_action_buffer = Super_Actions.PRE_JUMP
+				super_action_buffer_timer.start(SUPER_ACTION_BUFFER_TIMER_DURATION)
+
+		else:
+			get_next_action()
+
+
+func super_action_animations_process(delta):
+	if (next_action == Super_Actions.PRE_JUMP):
+		match (pre_jump_part):
+			0:
+				print("Pre jump part 1")
+				animation_node.frame = 1
+				animation_node.play(LAND_ANIMATION_NAME)
+				pre_jump_part1_time += delta
+
+				if (pre_jump_part1_time >= PRE_JUMP_PART1_DURATION):
+					pre_jump_part = (pre_jump_part + 1) % pre_jump_part_count
+					pre_jump_part1_time = 0
+
+
+			1:
+				print("Pre jump part 2")
+				animation_node.frame = 0
+				animation_node.play(LAND_ANIMATION_NAME)
+				pre_jump_part2_time += delta
+
+				if (pre_jump_part2_time >= PRE_JUMP_PART2_DURATION):
+					pre_jump_part = (pre_jump_part + 1) % pre_jump_part_count
+					pre_jump_part1_time = 0
+
+					next_action = Super_Actions.JUMP_SUPER_ACTION
+
+	if (next_action == Super_Actions.JUMP_SUPER_ACTION or next_action == Super_Actions.IS_JUMPING):
+		pass  # Leave the default animation (falling etc...) coded in animation_process
+
+
+
+func super_action_physics_process(_delta: float):
+	match (next_action):
+		Super_Actions.PRE_JUMP:
+			tmp_velocity = velocity
+			velocity = PRE_JUMP_VELOCITY_MULTIPLIER * tmp_velocity
+
+
+
 		Super_Actions.JUMP_SUPER_ACTION:
-
 			print("Super Action: ", super_action_names[Super_Actions.JUMP_SUPER_ACTION])
+			velocity.y = JUMP_VELOCITY
+			next_action = Super_Actions.IS_JUMPING
 
-	pass
+		Super_Actions.IS_JUMPING:
+			if (is_on_floor()):
+				get_next_action()
+
 
 
 #
@@ -142,14 +227,13 @@ func super_action_physics_process(action: Super_Actions, _delta: float):
 func _physics_process(delta: float):
 	capture_inputs()
 
+	if (tmp_velocity != Vector2.ZERO):
+		velocity = tmp_velocity
+		tmp_velocity = Vector2.ZERO
+
 	# Add the gravity.
 	if not is_on_floor():
 		acceleration.y = gravity 
-
-
-	# Handle Jump.
-	if Input.is_action_just_pressed(JUMP_ACTION_NAME) and (is_on_floor() or is_coyote_jump_available):
-		velocity.y = JUMP_VELOCITY
 
 
 	if is_moving_right:
@@ -162,21 +246,26 @@ func _physics_process(delta: float):
 		acceleration.x = direction * ACCEL_X - AIR_FRICTION_X * velocity.x
 	else:
 		acceleration.x = - AIR_FRICTION_X * velocity.x
-	
-
 		
+
 	# integrate acceleration
 	velocity += delta * acceleration
+
+	super_action_physics_process(delta)
 
 	move_and_slide()
 
 
 
-func _process(_delta):
-	animation_process()
+func _process(delta):
+	animation_process(delta)
 
 
 func _on_coyote_jump_timer_timeout():
 	is_coyote_jump_available = false
+
+
+func _on_super_action_buffer_timer_timeout():
+	super_action_buffer = Super_Actions.NO_SUPER_ACTION
 
 
