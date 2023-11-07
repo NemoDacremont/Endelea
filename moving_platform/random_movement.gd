3cdd799e04e45315fc1f2748de415f374a5bb4de
HORRUT Thomas <thomas.horrut@bordeaux-inp.fr>
2023-11-07 23:22:41 +0100

    🚧 not exploding time for oscillation

--- moving_platform/random_movement.gd
+++ moving_platform/random_movement.gd
@@ -4,6 +4,7 @@ var speed = 1;
 var oscillation_direction: Vector2;
 var random = RandomNumberGenerator.new();
 var t = 0;
+const TWO_PI = 2*PI;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,12 +13,12 @@ func _ready():
 	var random_angle = random.randf()*2*PI;
 	oscillation_direction = Vector2(cos(random_angle), sin(random_angle));
 	var speed = random.randf()*10;
-	t = random.randf()*2*PI;
+	t = random.randf()*TWO_PI;
 	print(speed/10);
-	print(random_angle/2/PI);
+	print(random_angle/TWO_PI);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	t += delta;
+	t = (t + delta)%TWO_PI;
 	move_and_collide(speed*oscillation_direction*cos(t))
