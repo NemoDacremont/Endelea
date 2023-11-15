4fb709c2af7108ebc5931a6c25431ade941699ef
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-15 16:22:44 +0100

    Add walljump

--- player/WallJump.gd
+++ player/WallJump.gd
@@ -18,18 +18,24 @@ func is_triggered() -> bool:
 	if (player_node.is_on_wall_only()):
 		# Wall jump to the right
 		if (Input.is_action_pressed(PlayerConstants.MOVE_LEFT_ACTION_NAME)):
-			wall_jump_direction = Vector2(1.5, -1)
+			wall_jump_direction = Vector2(-1, 1.5)
 
 		# Wall jump to the left
-		elif (Input.is_action_pressed(PlayerConstants.MOVE_LEFT_ACTION_NAME)):
-			wall_jump_direction = Vector2(-1.5, -1)
+		elif (Input.is_action_pressed(PlayerConstants.MOVE_RIGHT_ACTION_NAME)):
+			wall_jump_direction = Vector2(1, 1.5)
 
 		else:
+			print("Walljump: no direction")
 			return false
 
+		wall_jump_direction = wall_jump_direction.normalized()
 		return Input.is_action_just_pressed(PlayerConstants.JUMP_ACTION_NAME)
 
 	return false
+
+
+func can_start():
+	return player_node.is_on_wall_only()
 
 
 func start():
@@ -39,11 +45,11 @@ func start():
 
 func get_to_next_state():
 	if wall_jump_state == Wall_Jump_State.PRE_WALL_JUMP:
-		# wall_jumping.start()
 		wall_jump_state = Wall_Jump_State.WALL_JUMPING
+		wall_jumping.start()
 
 	elif wall_jump_state == Wall_Jump_State.WALL_JUMPING:
-		state_machine.state = state_machine.States.IDLE
+		state_machine.push_state(state_machine.States.IDLE)
 		wall_jump_state = Wall_Jump_State.NONE
 
 
@@ -55,14 +61,11 @@ func physics_process(delta: float, player: CharacterBody2D):
 	match (wall_jump_state):
 		Wall_Jump_State.PRE_WALL_JUMP:
 			pre_wall_jump.physics_process(delta, player)
-			pass
 
 		Wall_Jump_State.WALL_JUMPING:
 			wall_jumping.physics_process(delta, player)
-			pass
 
 		Wall_Jump_State.NONE:  # isn't supposed to happen
-			print("Wall Jump in NONE state, not supposed to happen")
 			pass
 
 
@@ -71,9 +74,21 @@ func physics_process(delta: float, player: CharacterBody2D):
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func process(_delta: float, _player: CharacterBody2D):
-	pass
+func process(delta: float, player: CharacterBody2D):
+	match (wall_jump_state):
+		Wall_Jump_State.PRE_WALL_JUMP:
+			pre_wall_jump.process(delta, player)
 
+		Wall_Jump_State.WALL_JUMPING:
+			wall_jumping.process(delta, player)
+
+		Wall_Jump_State.NONE:  # isn't supposed to happen
+			print("Wall Jump in NONE state, not supposed to happen")
+			pass
+
+
+func background_process():
+	pass
 
 
 
--- player/WallJumping.gd
+++ player/WallJumping.gd
@@ -1,11 +1,11 @@
 extends Node
 
 
+# Nodes
 @onready var wall_jump: Node = get_parent()
 @onready var player_node: CharacterBody2D = wall_jump.get_parent().get_parent()
-@onready var wall_jump_timer: Timer = $WallJumpTimer
+@onready var wall_jump_timer: Timer = get_node("../WallJumpTimer")
 
-static var are_movements_blocked: bool = false
 
 # Copy pasted from idle
 static var is_moving_right: bool = false
@@ -20,48 +20,22 @@ static var default_gravity_force: Vector2 = PlayerConstants.GRAVITY * Vector2.DO
 static var gravity_force: Vector2 = PlayerConstants.GRAVITY * Vector2.DOWN
 
 
-func capture_inputs():
-	is_moving_right = false
-	is_moving_left = false
-
-	if (Input.is_action_pressed(PlayerConstants.MOVE_RIGHT_ACTION_NAME)):
-		is_moving_right = true
-
-	elif (Input.is_action_pressed(PlayerConstants.MOVE_LEFT_ACTION_NAME)):
-		is_moving_left = true
-
-
-
 func start():
 	player_node.velocity = PlayerConstants.JUMP_VELOCITY * wall_jump.wall_jump_direction
-	are_movements_blocked = true
+	wall_jump_timer.start(PlayerConstants.WALL_JUMP_BLOCKING_DURATION)
 
 
 
 func physics_process(delta: float, player: CharacterBody2D):
-	capture_inputs()
-	print(wall_jump.wall_jump_direction)
+	player.acceleration = Vector2.ZERO  # Recalculate on every frames
 
-
-	player.acceleration = Vector2.ZERO
-	# Almost entirely copy paster from idle (just slowed)
-	# Update run force direction
-	movement_direction = 0
-
-	if (is_moving_right):
-		movement_direction = 1
-	elif (is_moving_left):
-		movement_direction = -1
-
-	if (are_movements_blocked):
-		movement_direction = 0
-
+	movement_direction = wall_jump.wall_jump_direction.x
 
 	# Update forces
 	run_force = movement_direction * PlayerConstants.ACCEL_X * Vector2.RIGHT
-	air_friction_force.x = - PlayerConstants.AIR_FRICTION_X * player.velocity.x
+	air_friction_force.x = - PlayerConstants.AIR_FRICTION_X * player.velocity.x / 10
 
-	# gravity is higher if falling
+	# gravity is higher when falling
 	gravity_force = default_gravity_force
 	if (player.velocity.y > 0):
 		gravity_force *= PlayerConstants.FALLING_GRAVITY_MULTIPLIER
@@ -73,7 +47,7 @@ func physics_process(delta: float, player: CharacterBody2D):
 		player.acceleration += gravity_force
 
 
-	# Integrate
+	# Integrate accel and cap falling speed
 	player.velocity += player.acceleration * delta
 	player.velocity.y = min(player.velocity.y, PlayerConstants.MAX_FALL_SPEED)
 
@@ -81,16 +55,14 @@ func physics_process(delta: float, player: CharacterBody2D):
 	# godot doing godot thigns
 	player.move_and_slide()
 
-	if (player.is_on_floor() or player.is_on_wall()):
-		wall_jump.get_to_next_state()
-
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func process(_delta: float, _player: CharacterBody2D, _idle: Node):
+func process(_delta: float, _player: CharacterBody2D):
 	pass
 
 
 func _on_wall_jump_timer_timeout():
-	are_movements_blocked = false
+	wall_jump.get_to_next_state()
+
 
