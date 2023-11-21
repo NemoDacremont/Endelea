2218d0d353964ad287d355a2c2eb278dcc9df2cc
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-21 21:21:16 +0100

    Moved mainmenu

--- main_menu/MainMenu.gd
+++ main_menu/MainMenu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://controls_screen/controls_screen.tscn")
 
 
 func _on_quit_pressed():
