a829881b5c4801d982a49ab98f2291c11deec5ec
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-23 19:37:13 +0100

    Add fog shader here and there, better tuto map

--- tutorial_level/tutorial_level.gd
+++ tutorial_level/tutorial_level.gd
@@ -2,14 +2,21 @@ extends Control
 
 const FADING_DURATION: float = 1
 
-var fading: ColorRect
+@onready var pause_screen = $UI/Pause_Screen
+
 var fading_tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var fading = $UI/Control/Fading
+	fading.color = Color(0.875, 0.973, 1, 1)
 	fading_tween = get_tree().create_tween();
-	fading = $Fading
 	fading.visible = true  # set to false in editor so can work on the scene
 
 	fading_tween.tween_property(fading, "color", Color(0.875, 0.973, 1, 0), FADING_DURATION)
+
+
+
+func _on_player_pause():
+	pause_screen.visible = !pause_screen.visible
 
