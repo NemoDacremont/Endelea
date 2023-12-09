828408b410f960a00da16ff39823a317aa37f183
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2023-12-09 11:07:21 +0100

    Create little animation for collecting coin

--- coin/coin.gd
+++ coin/coin.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+const TAKEN_ANIMATION_NAME: String = "taken"
+# const IDLE_ANIMATION_NAME: String = "idle"
+
+
+
+
 func _on_body_shape_entered(_body_rid:RID, _body:Node2D, _body_shape_index:int, _local_shape_index:int):
 	Global.number_of_coins += 1
-	hide()
+
+	animation_player.play(TAKEN_ANIMATION_NAME)
 
 
